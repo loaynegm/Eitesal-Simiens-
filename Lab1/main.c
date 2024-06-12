a38a56da56487d032c470510da2249dc929c797d
loaynegm <160424235+loaynegm@users.noreply.github.com>
2024-06-12 14:41:22 +0300

    Update main.c

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,1 +1,42 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 18, 2023
+ *      Author: loay_negm
+ */
 
+//----------------------------------------------- Include Files ------------------------------------
+#include "stdint.h"
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include <Pin.h>
+
+//---------------------------------------------- Macros --------------------------------------------
+
+//---------------------------------------------- Variables -----------------------------------------
+
+//-------------------------------------- Function Decelerations ------------------------------------
+void DelayMS(uint32_t delayValueMs);
+
+
+void main(void){
+    SysCtlClockSet(SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ | SYSCTL_USE_PLL | SYSCTL_SYSDIV_2_5);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
+    while(1){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0b1000);
+        DelayMS(500);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
+        DelayMS(500);
+    }
+}
+
+
+void DelayMS(uint32_t delayValueMs){
+    SysCtlDelay(((float)80000000.0 * (float)delayValueMs)/6000.0);
+}
